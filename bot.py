2e630894888e75d7835b71bf9bee346300187ae8
adarshkumar714 <adarshkumar20012704@gmail.com>
2023-10-28 15:57:15 +0530

    started improving commands usage

--- bot.py
+++ bot.py
@@ -31,7 +31,7 @@ from telegram.ext import (
 from telegram.constants import ParseMode, ChatAction
 
 
-from gpt import get_response
+# from gpt import get_response
 
 from dotenv import load_dotenv
 load_dotenv()
@@ -40,35 +40,72 @@ start_reply_text = """
 Hey There!
 
 Commands:
-/start
-/ask [query]
+⚪ /start
+🤖 /ask
 """
+
+global gpt_mode
+
 async def start(update: Update, context: CallbackContext):
     print('[+] /start')
+    gpt_mode = False
     await update.message.reply_text(start_reply_text)
 
 async def ask(update: Update, context: CallbackContext):
-    message = update.message.text[5:]
-    print(f'[+] /ask "{message}"')
+    try:
+        print(gpt_mode)
+    except:
+        gpt_mode = False
+    if gpt_mode:
+        gpt_mode = False
+        await update.message.reply_text('GPT Mode Turned Off')
+    else:
+        gpt_mode = True
+        await update.message.reply_text('GPT Mode Turned On')
+
+async def gpt_chat_handler(update: Update, context: CallbackContext):
+    message = update.message.text
+    print(f'[+] gpt query: "{message}"')
     if message=="":
         await update.message.reply_text('Query is empty, Please provide some query!')
         return
     await update.message.chat.send_action(action="typing")
     placeholder_message = await update.message.reply_text('generating...')
-    response = await get_response(message)
+    # response = await get_response(message)
+    response = {
+        'choices':[{
+            'message':{'content':'asdf'},
+            'related_links':['no related links']
+        }]
+    }
     await update.message.chat.send_action(action="typing")
     await context.bot.edit_message_text(response['choices'][0]['message']['content'], chat_id=placeholder_message.chat_id, message_id=placeholder_message.message_id)
     for _ in response['choices'][0]['related_links']:
         await update.message.reply_text(_)
-    
+    await update.message.reply_text('<i>*turn off gpt mode: /ask</i>', parse_mode=ParseMode.HTML)
 
 async def message_handler(update: Update, context: CallbackContext, message=None):
     print('[+] message_handler :', update.message.text)
     await update.message.chat.send_action(action="typing")
     await update.message.reply_text('message: '+update.message.text)
+    gpt_mode = True
+    if gpt_mode:
+        await gpt_chat_handler(update, context)
+    else:
+        await update.message.reply_text('GPT Mode is Off!\nplease use /ask to turn On of Off')
+
+async def post_init(application: Application):
+    await application.bot.set_my_commands([
+        BotCommand("/start", "Start Bot"),
+        BotCommand("/ask", "Turn GPT Mode On or Off")
+    ])
 
 def run_bot() -> None:
-    application = ApplicationBuilder().token(os.getenv('TELEGRAM_BOT_TOKEN')).build()
+    application = (ApplicationBuilder()
+        .token(os.getenv('TELEGRAM_BOT_TOKEN'))
+        .post_init(post_init)
+        .build()
+    )
 
     application.add_handler(CommandHandler('start', start))
     application.add_handler(CommandHandler('ask', ask))
